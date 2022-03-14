3e57d2802011e5a4e844ecbbce862c7c2d6bfff3
Irfan3006 <92904571+Irfan3006@users.noreply.github.com>
2022-03-14 17:02:03 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+
+// ================= START BOT CODE ===================
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.on("ready", () => {
+    client.user.setActivity("Speed", { type: "WATCHING"})
+})
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong!');
+  }
+});
+// You really don't want your token here since your repl's code
+// is publically available. We'll take advantage of a Repl.it 
+// feature to hide the token we got earlier. 
+
+
+client.login('PLACE_TOKEN_HERE');
